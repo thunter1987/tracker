bf56bb7d8eb5018ddd78f2912d10bca7d1b12fac
Tony Hunter <thunter1987@yahoo.com>
2023-11-12 23:41:57 -0600

    built navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+import {BsBugFill} from 'react-icons/bs'
+
+const NavBar = () => {
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'},
+    ]
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href='/'><BsBugFill /></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => <Link 
+            key={link.href} 
+            className='text-zinc-500 hover:text-zinc-900 transition-colors' 
+            href={link.href}>{link.label}</Link>
+)}
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
